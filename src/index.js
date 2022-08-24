94655e48cbfaecfc47f972115f300fa75f94eac1
Celio Junior <celio_cjr@hotmail.com>
2022-08-24 12:15:38 -0300

    made the post request to create a customer and the rule to not create a new model if already exists

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "oieeee" });
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - array
+ */
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if(customerAlreadyExists) {
+    return response.status(400).json({error: "Cliente já existe."})
+  }
+
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return response.status(201).send();
 })
 
 app.listen(3333);
