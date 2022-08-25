f6804d4f76b8f0e217d40fd690cbd5599ba56051
Celio Junior <celio_cjr@hotmail.com>
2022-08-25 10:22:18 -0300

    fix the issue to check if cpf already exists

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-import express from 'express';
+import express from 'express'
+import { v4 as uuidv4 } from 'uuid'
+
+const app = express()
+
+
+const customers = []
+/**
+ * - MODEL: 
+ * cpf = string
+ * name = string
+ * id = uuid
+ * statement = []
+ */
+
+app.use(express.json())
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body
+
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+  if(customerAlreadyExists) {
+    res.status(400).json({ error: "Customer already exists" })
+  }
+
+  const id = uuidv4()
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  })
+
+  return res.status(201).send()
+
+})
+
+app.listen(3333)
