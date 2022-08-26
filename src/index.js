bfc85adfcbf4a06f7ed0af041f1c13c06fa50651
Celio Junior <celio_cjr@hotmail.com>
2022-08-26 17:24:44 -0300

    made the middlware to verify if the cpf exists

--- src/index.js
+++ src/index.js
@@ -5,6 +5,23 @@ const app = express()
 
 
 const customers = []
+// função Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers
+
+  const customer = customers.find((customer => customer.cpf === cpf))
+  
+  if(!customer) {
+    return res.status(400).json({ error: "Customer not found" })
+  }
+
+  // Fazendo o middleware ter acesso ao request customer
+  req.customer = customer
+
+  return next()
+
+}
+
 /**
  * - MODEL: 
  * cpf = string
@@ -15,6 +32,11 @@ const customers = []
 
 app.use(express.json())
 
+// Incluindo o middleware em todas as rotas:
+// app.use(verifyIfExistsAccountCPF)
+
+
+// Inserindo os dados de um cliente
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body
 
@@ -34,6 +56,13 @@ app.post("/account", (req, res) => {
   })
 
   return res.status(201).send()
+
+})
+
+// Buscando o extrato de um cliente de acordo com seu CPF
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req
+  return res.json(customer.statement)
 
 })
 
