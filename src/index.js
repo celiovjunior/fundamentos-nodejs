55a137b228a4222801ecfde1db5a98020f932384
Celio Junior <celio_cjr@hotmail.com>
2022-08-26 19:54:34 -0300

    made the logic to get the statement according to the date

--- src/index.js
+++ src/index.js
@@ -22,6 +22,23 @@ function verifyIfExistsAccountCPF(req, res, next) {
 
 }
 
+// função para checar o saldo e pegar o balanço da conta
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+
+      return acc + operation.amount
+    
+    } else {
+
+      return acc - operation.amount
+
+    }
+  }, 0)
+
+  return balance
+}
+
 /**
  * - MODEL: 
  * cpf = string
@@ -64,6 +81,60 @@ app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
   const { customer } = req
   return res.json(customer.statement)
 
+})
+
+// Depósito
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+  const { description, amount } = req.body
+
+  const { customer } = req
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation)
+
+  return res.status(201).send()
+})
+
+// Saque
+app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
+  const { amount } = req.body
+
+  const { customer } = req
+
+  const balance = getBalance(customer.statement)
+
+  if(balance < amount) {
+    return res.status(400).json({error: "Operation not allowed"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  }
+
+  customer.statement.push(statementOperation)
+
+  return res.send(201).send()
+})
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req
+
+  const { date } = req.query
+
+  const dateFormat = new Date(date +  " 00:00")
+
+  // Algoritmo que vai formatar a data de acordo com o dateFormat e depois comparar com a data do created_at no formato de String:
+  const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+  return res.json(statement)
 })
 
 app.listen(3333)
